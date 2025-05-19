63bbe116bad4adb7c6ba5eb963daf579327931e0
Arom <arom.sanchez@tecsup.edu.pe>
2025-05-19 17:08:02 -0500

    Agregue el slug, para categorias, automaticamente se generaria el slug segun el nombre

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -13,5 +14,15 @@ class Category extends Model
     protected $fillable = [
         'name',
         'description',
+        'slug',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            $category->slug = \Illuminate\Support\Str::slug($category->name);
+        });
+    }
 }
--- database/migrations/2025_05_19_005142_create_category_table.php
+++ database/migrations/2025_05_19_005142_create_category_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 100);
             $table->string('description', 255)->nullable();
+            $table->string('slug', 255)->unique();
             $table->timestamps();
         });
     }
